9e6808bbb710c65041ea0d1a2b27f875558380d6
NismoR35-M <nismoms55@gmail.com>
2023-07-04 09:30:50 +0300

    Login function with routes

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,7 +10,32 @@ use Spatie\Activitylog\Models\Activity;
 
 class AdminController extends Controller {
     public function login(Request $request){
-        
+        //validate login data
+        $validateData = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        // Attempt to authenticate the admin credentials
+        if (Auth::guard('admin')->attempt($validatedData)) {
+            // Get the authenticated admin
+            $admin = Auth::guard('admin')->user();
+
+            // Store the admin's ID in the session
+            Auth::guard('admin')->loginUsingId($admin->id);
+
+            // Log the admin login activity
+            activity()
+                ->performedOn($admin)
+                ->causedBy($admin)
+                ->log('Admin Login');
+
+            // Redirect to the admin dashboard or any other desired page
+            return redirect()->route('admin.dashboard');
+        }
+
+        // Authentication failed, redirect back to the login form with an error message
+        return redirect()->back()->withErrors(['message' => 'Invalid credentials']);
 
     }
 }
