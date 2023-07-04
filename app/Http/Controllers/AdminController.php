0aa153033363b85363cf685bf258bc2863aa0e89
NismoR35-M <nismoms55@gmail.com>
2023-07-04 09:25:23 +0300

    "AdminController Authentication"

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\Admin;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use AuthenticatesAdmins;
+use Spatie\Activitylog\Models\Activity;
+
+
+class AdminController extends Controller {
+    public function login(Request $request){
+        
+
+    }
+}
