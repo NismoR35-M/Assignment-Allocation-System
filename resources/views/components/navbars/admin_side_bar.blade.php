c5ed02cd8a59b598f4e7781b67351679bf180ec6
RivoltaAlpha <mwanikitiffany25@gmail.com>
2023-07-05 17:20:36 +0300

    member

--- resources/views/components/navbars/admin_side_bar.blade.php
+++ resources/views/components/navbars/admin_side_bar.blade.php
@@ -59,7 +59,17 @@
                 </a>
             </li>
             
+            <li class="nav-item">
+                <a class="nav-link text-white {{ $activePage == 'tables' ? ' active bg-gradient-primary' : '' }} "
+                    href="{{ route('show_users') }}">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="material-icons opacity-10">table_view</i>
+                    </div>
+                    <span class="nav-link-text ms-1">Members</span>
+                </a>
+            </li>
             
+
             <li class="nav-item">
                 <a class="nav-link text-white " href="{{ route('static-sign-in') }}">
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
