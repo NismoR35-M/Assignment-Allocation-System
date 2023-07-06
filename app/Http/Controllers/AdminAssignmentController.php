91e8d08c759691a28c55afc354134c6e50b826f3
NismoR35-M <nismoms55@gmail.com>
2023-07-06 13:39:42 +0300

    Some changes

--- app/Http/Controllers/AdminAssignmentController.php
+++ app/Http/Controllers/AdminAssignmentController.php
@@ -41,44 +41,49 @@ class AdminAssignmentController extends Controller
     }
 
     public function assignAssignment(Request $request)
-    {
-        // Validate the form data
-        $validatedData = $request->validate([
-            'name' => 'required',
-            'company_name' => 'required',
-            'request_type' => 'required',
-            'description' => 'required',
-            'start_date' => 'required',
-            'status' => 'required',
-            'response' => 'nullable',
-            'response_file' => 'nullable|file',
-        ]);
+{
+    // Validate the form data
+    $validatedData = $request->validate([
+        'name' => 'required',
+        'company_name' => 'required',
+        'request_type' => 'required',
+        'description' => 'required',
+        'start_date' => 'required',
+        'status' => 'required',
+        'response' => 'nullable',
+        'response_file' => 'nullable|file',
+    ]);
 
-        // Create a new Assignment instance
-        $assignment = new Assignment();
+    // Create a new Assignment instance
+    $assignment = new Assignment();
 
-        // Assign form data to the Assignment instance
+    // Assign form data to the Assignment instance
+    $assignment->name = $request->input('name');
+    $assignment->company_name = $request->input('company_name');
+    $assignment->request_type = $request->input('request_type');
+    $assignment->description = $request->input('description');
+    $assignment->start_date = $request->input('datereceived'); // Update to 'datereceived' instead of 'start_date'
+    $assignment->status = $request->input('status');
+    $assignment->response = $request->input('response');
 
-        $assignment->name = $request->input('name');
-        $assignment->company_name = $request->input('company_name');
-        $assignment->request_type = $request->input('request_type');
-        $assignment->description = $request->input('description');
-        $assignment->start_date = $request->input('start_date');
-        $assignment->status = $request->input('status');
-        $assignment->response = $request->input('response');
-
-        // Upload and store the response file, if provided
-        if ($request->hasFile('response_file')) {
-            $file = $request->file('response_file');
-            $path = $file->store('response_files', 'public');
-            $assignment->response_file = $path;
-        }
-
-        // Save the Assignment instance to the database
-        $assignment->save();
-        $assignment->users()->sync($validatedData['user']);
-        // Redirect or perform any other actions after saving the assignment
-        return view('admin.assign_assignments', compact('users'))->with('success', 'Assignment saved successfully!');
-        // Return a response
+    // Upload and store the response file, if provided
+    if ($request->hasFile('response_file')) {
+        $file = $request->file('response_file');
+        $path = $file->store('response_files', 'public');
+        $assignment->response_file = $path;
     }
+
+    // Save the Assignment instance to the database
+    $assignment->save();
+
+    // Retrieve the selected user IDs
+    $userIds = $request->input('user');
+    
+    // Attach the users to the assignment in the junction table
+    $assignment->users()->attach($userIds);
+
+    // Redirect or perform any other actions after saving the assignment
+    return redirect()->back()->with('success', 'Assignment saved successfully!');
+}
+
 }
